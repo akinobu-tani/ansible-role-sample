d46201faa3fb3d14a98d9763c09b888f70e3cf00
akinobu.tani <akinobu.x.tani@gmail.com>
2017-02-03 07:21:20 +0900

    change spec

--- test/integration/default/serverspec/sample_spec.rb
+++ test/integration/default/serverspec/sample_spec.rb
@@ -5,12 +5,3 @@ set :backend, :exec
 describe package('httpd') do
   it { should be_installed }
 end
-
-describe service('httpd') do
-  it { should be_enabled }
-  it { should be_running }
-end
-
-describe port(80) do
-  it { should be_listening }
-end
